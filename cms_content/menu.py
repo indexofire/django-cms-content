6d4c2b320ac63818dbe9ec5fb5004e7505b25ccd
Mark Renton <indexofire@gmail.com>
2010-05-31 15:44:19 +0800

    add ugly mode to breadcrumb

--- cms_content/menu.py
+++ cms_content/menu.py
@@ -1,21 +1,49 @@
 # -*- coding: utf-8 -*-
-from menus.base import Menu, NavigationNode
-from menus.menu_pool import menu_pool
 from django.utils.translation import ugettext as _
 from django.core.urlresolvers import reverse, NoReverseMatch
 
 from cms.menu_bases import CMSAttachMenu
 from cms.app_base import CMSApp
-from cms_content.models import CMSSection, CMSCategory
+from menus.base import Menu, NavigationNode
+from menus.menu_pool import menu_pool
+
+from cms_content.models import CMSSection, CMSCategory, CMSArticle
 
 class CMSSectionMenu(Menu):
     """CMS Section Menu
     Create a section list menu in pages.
     """
+    #name = _("CMS Content")
+
     def get_nodes(self, request):
         nodes = []
+        count = 1
+
         for section in CMSSection.objects.all():
-            nodes.append(NavigationNode(section.name, section.get_absolute_url(), 'cms_content', section.pk))
+            nodes.append(NavigationNode(section.name, '/cms/'+section.get_absolute_url(), section.pk))
+            count += 1
+            for category in CMSCategory.objects.select_related('section').all():
+                num = count
+                nodes.append(NavigationNode(category.name, '/cms/'+section.get_absolute_url()+category.get_absolute_url(), num, category.section.pk))
+                count += 1
+                for article in CMSArticle.objects.select_related('category').all():
+                    nodes.append(NavigationNode(article.title, '/cms/'+section.get_absolute_url()+category.get_absolute_url()+article.get_absolute_url(), count, num))
+                    count += 1
+                    
+
+        #for section in CMSSection.objects.all():
+        #    nodes.append(NavigationNode(section.name, section.get_absolute_url(), section.pk))
+        #    category = CMSCategory.objects.select_related().get(id=1)
+        #    print category, "\n"
+        #    article = CMSArticle.objects.select_related().get(pk=)
+
+            #nodes.append(NavigationNode(category.name, category.get_absolute_url(), category.pk, section.pk))
+        #n1 = NavigationNode(_('Python'), "/cms/python/", 1)
+        #n2 = NavigationNode(_('Django'), "/cms/python/django/", 2, 1)
+        #n3 = NavigationNode(_('Override save_model on Django InlineModelAdmin'), "/cms/python/django/1/", 3, 2)
+        #nodes.append(n1)
+        #nodes.append(n2)
+        #nodes.append(n3)
         return nodes
 
 class CMSCategoryMenu(Menu):
