8cf4d0de5d4ced790678ae58e36a98d272e891e0
Mark Renton <indexofire@gmail.com>
2010-06-01 12:37:45 +0800

    remove wasted query

--- cms_content/menu.py
+++ cms_content/menu.py
@@ -22,28 +22,14 @@ class CMSSectionMenu(Menu):
         for section in CMSSection.objects.all():
             nodes.append(NavigationNode(section.name, '/cms/'+section.get_absolute_url(), section.pk))
             count += 1
-            for category in CMSCategory.objects.select_related('section').all():
+            for category in CMSCategory.objects.select_related('section').filter(section__pk=section.pk):
                 num = count
                 nodes.append(NavigationNode(category.name, '/cms/'+section.get_absolute_url()+category.get_absolute_url(), num, category.section.pk))
                 count += 1
-                for article in CMSArticle.objects.select_related('category').all():
+                for article in CMSArticle.objects.select_related('category').filter(category__pk=category.pk):
                     nodes.append(NavigationNode(article.title, '/cms/'+section.get_absolute_url()+category.get_absolute_url()+article.get_absolute_url(), count, num))
                     count += 1
-                    
 
-        #for section in CMSSection.objects.all():
-        #    nodes.append(NavigationNode(section.name, section.get_absolute_url(), section.pk))
-        #    category = CMSCategory.objects.select_related().get(id=1)
-        #    print category, "\n"
-        #    article = CMSArticle.objects.select_related().get(pk=)
-
-            #nodes.append(NavigationNode(category.name, category.get_absolute_url(), category.pk, section.pk))
-        #n1 = NavigationNode(_('Python'), "/cms/python/", 1)
-        #n2 = NavigationNode(_('Django'), "/cms/python/django/", 2, 1)
-        #n3 = NavigationNode(_('Override save_model on Django InlineModelAdmin'), "/cms/python/django/1/", 3, 2)
-        #nodes.append(n1)
-        #nodes.append(n2)
-        #nodes.append(n3)
         return nodes
 
 class CMSCategoryMenu(Menu):
