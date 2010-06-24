fd7577d34ef206869517f3717070880d098d4d8b
Mark Renton <indexofire@gmail.com>
2010-06-24 14:38:48 +0800

    change URL dispach rules

--- cms_content/urls.py
+++ cms_content/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns ('',
     url(r'^$', section_list, {'sections': CMSSection.objects.all()}, name='section'),
     url(r'^(?P<slug>\w*)/$', category_list, name='category_list'),
     url(r'^(?P<slug>\w*)/(?P<path>\w*)/$', article_list),
-    url(r'^(?P<slug>\w*)/(?P<path>\w*)/(?P<name>[\w-]*)/$', article_view),
+    url(r'^(?P<slug>[-\w]+)/(?P<path>[-\w]+)/(?P<name>[-\w]+)/$', article_view),
 )
