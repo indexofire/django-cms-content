53e03570cd520c26f49f0cbfdd911eb554d2a7b1
Mark Renron <indexofire@gmail.com>
2010-06-29 23:53:13 +0800

    add front end admin

--- cms_content/__init__.py
+++ cms_content/__init__.py
@@ -1,1 +1,31 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+models.Model._admin_url_name = lambda self, type: 'admin:%s_%s_%s' % (
+    self._meta.app_label, self._meta.module_name, type)
+
+def get_admin_change_url(self):
+    return reverse(self._admin_url_name('change'), args=(self.pk, ))
+models.Model.get_admin_change_url = get_admin_change_url
+
+def get_admin_delete_url(self):
+    return reverse(self._admin_url_name('delete'), args=(self.pk, ))
+models.Model.get_admin_delete_url = get_admin_delete_url
+
+def get_admin_history_url(self):
+    return reverse(self._admin_url_name('history'), args=(self.pk, ))
+models.Model.get_admin_history_url = get_admin_history_url
+
+def get_admin_changelist_url(self):
+    return reverse(self._admin_url_name('changelist'))
+models.Model.get_admin_changelist_url = get_admin_changelist_url
+
+def get_admin_add_url(self):
+    return reverse(self._admin_url_name('add'))
+models.Model.get_admin_add_url = get_admin_add_url
+
+models.Model.get_verbose_name = lambda self: self._meta.verbose_name
+models.Model.get_verbose_name_plural = lambda self: self._meta.verbose_name_plural
+
 version = ('0', '0', '2', 'alpha')
+
---
+++ cms_content/middleware.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class FrontEndPublishMiddleware(object):
+    """
+    Middleware to add function of front-end publish articles.
+    The action is copy from django-debug-toolbar
+    """
+    #def _show_frontend_editor(self, request):
+        
+    
+    
+    def process_request(self, request):
+        print ''
+    
+    #def process_view(self, request, view_func, view_args, view_kwargs):
+    
+    
+    #def process_response(self, request, response):
+        
+    
+    
